5ae356ce291270b7b1f6d0183ab9a1cd3be2a24c
Larissa Severo <larissa.proenca@fatec.sp.gov.br>
2026-03-07 23:18:21 -0300

    chore: initial solution API VetPawPlatform

---
+++ README.md
@@ -0,0 +1,60 @@
+\# 🐾 VetPaw Platform API
+
+
+
+API para a Plataforma de gestão veterinária.
+
+
+
+\## 🚀 Stack
+
+
+
+\### Backend
+
+\- \*\*.NET 8\*\*
+
+\- \*\*ASP.NET Core Web API\*\*
+
+\- \*\*C#\*\*
+
+\- \*\*DDD + Clean Architecture\*\*
+
+\- \*\*SOLID Principles\*\*
+
+
+
+\### Banco de Dados
+
+\- \*\*Amazon DynamoDB\*\* (NoSQL, alta escalabilidade)
+
+
+
+\### Documentação da API
+
+\- \*\*Swagger / OpenAPI\*\*
+
+
+
+\### Testes
+
+\- \*\*xUnit\*\*
+
+\- Testes unitários
+
+\- Testes de integração
+
+
+
+\## 🧱 Arquitetura
+
+src
+
+├─ Api
+
+├─ Application
+
+├─ Domain
+
+└─ Infra
+
---
+++ VetPawPlatformAPI/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AngularPolicy");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
